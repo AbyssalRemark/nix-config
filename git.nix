71a812c939227f25872ddd5278aec6fbd19c5228
AbyssalRemark <dev.dominicsevere@gmail.com>
2024-06-25 18:20:58 -0700

    added lg alias to git and added thing to make the upstream problem go away

--- git.nix
+++ git.nix
@@ -6,6 +6,13 @@
       enable = true;
       userEmail = "dominicsevere@gmail.com";
       userName = "AbyssalRemark";
+      lfs.enable = true;
+      extraConfig = {
+        push.default = "current";
+      };
+      aliases = { 
+        lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+      };
     };
     gitui.enable = true;
   };
